a45cb7db74774e88276c84909f07720aaa580549
FildCommander <git@paul-stoeckel.de>
2024-10-07 22:01:12 +0200

    Initial commit

---
+++ src/main/java/cc/neuanfang/basic_economy/BasicEconomy.java
@@ -0,0 +1,16 @@
+package cc.neuanfang.basic_economy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicEconomy implements ModInitializer {
+	public static final String MOD_ID = "basic_economy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/cc/neuanfang/basic_economy/BasicEconomyClient.java
@@ -0,0 +1,11 @@
+package cc.neuanfang.basic_economy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicEconomyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
